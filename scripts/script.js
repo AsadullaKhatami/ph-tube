571438dea2f123cf33ec6cda6a24279d84975343
Asadulla Khatami <asadullakhatami@gmail.com>
2025-09-04 14:30:47 +0600

    categories section is loaded from api

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,35 @@
-console.log("hello !");
+// console.log("hello !");
+
+const loadCategories = () => {
+    fetch('https://openapi.programming-hero.com/api/phero-tube/categories')
+    .then(response => response.json())
+    .then(data => displayCategories(data.categories))
+    .catch(err => console.log(err));
+}
+
+// fetch video 
+const loadVideo = () => {
+    fetch('https://openapi.programming-hero.com/api/phero-tube/videos')
+    .then(response => response.json())
+    .then(data => displayVideo(data))
+    .catch(err => console.log(err));
+}
+
+// display categories
+const displayCategories = (data) => {
+    const categoriesBtn = document.getElementById('categories');
+    for(let categories of data){
+        // console.log(categories.category);
+        const button = document.createElement('button');
+        button.classList.add('btn');
+        button.innerText = categories.category;
+
+        categoriesBtn.append(button);
+    }
+}
+
+const displayVideo = (data) => {
+    console.log(data);
+}
+
+loadCategories();
